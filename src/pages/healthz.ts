9ae43acbbf8ed789b6cabfbfafc7124532469c17
Spuxx <ameisenfutter@outlook.com>
2023-05-06 16:23:40 +0200

    formatting

--- src/pages/healthz.ts
+++ src/pages/healthz.ts
@@ -1,4 +1,4 @@
-import type { APIRoute } from "astro";
+import type { APIRoute } from 'astro';
 export const get: APIRoute = async function get() {
-  return { body: JSON.stringify({ status: "ok" }) };
+  return { body: JSON.stringify({ status: 'ok' }) };
 };
